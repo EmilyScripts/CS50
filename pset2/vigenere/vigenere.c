cac6eef313b6b74c0ea95493239172ff38e78f69
EmilyScripts <32196496+EmilyScripts@users.noreply.github.com>
2018-03-17 12:39:29 +0000

    Update vigenere.c

--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -7,14 +7,20 @@
 int main(int argc, string argv[])
 {
 
+//check two arguments are given
+
 if (argc != 2) {
 
     printf("Need a key!\n");
     return 1;
 }
 
+//store key as string and get length of key
+
 string key = argv[1];
 int keylength = strlen(key);
+
+//check key is all alphabetical letters
 
 for (int i = 0; i < keylength; i++) {
 
@@ -23,31 +29,40 @@ for (int i = 0; i < keylength; i++) {
         printf("Must be alphabetical letters only!\n");
         return 1;
     }
-
 }
+
+//get text to encode and print ciphertext
 
 string text = get_string("plaintext: ");
 printf("ciphertext: ");
 
+//reject if no text is given
+
 if (text == NULL) {
-    return 0;
+    return 1;
 }
 
+//loop through text to change to ciphertext
 
 for (int i = 0, j = 0; i < strlen(text); i++) {
 
-    int n = key[(j % keylength)];
+//get letter of key needed, used to loop through key as many times as needed to cipher text
 
+    int n = tolower(key[j % keylength]) - 'a';
+
+//maintain correct case of letter
 
     if (isupper(text[i])) {
 
-        printf("%c", ((text[i] - 'A' + (n - 'A')) % 26) + 'A');
+//add key to letter of text and increment key letter index if used
+
+        printf("%c", 'A' + (text[i] - 'A' + n) % 26);
         j++;
     }
 
     else if (islower(text[i])) {
 
-        printf("%c", ((text[i] - 'a' + (n - 'a')) % 26) + 'a');
+        printf("%c", 'a' +(text[i] - 'a' + n) % 26);
         j++;
     }
 
